c19b7966fec3b58f2248a7996f422ce4cd396acd
nahum22 <nahum.nahum@gmail.com>
2024-07-06 21:04:45 +0300

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import FirstNavbar from "./FirstNavbar";
 import Footer from "./Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CarouselPage from "./CarouselPage";
-import React from "react"; // Removed useEffect and useState since they're not used
-import { collection, getDocs } from "firebase/firestore"; // Correct imports for Firestore
-import { db } from "./firebase_setup/firebase"; // Ensure this path is correct
 
 import LandingPage from "./LandingPage";
 import Admin from "./Admin";
