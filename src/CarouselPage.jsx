1bc3d86492342c860926909edb434a2371058511
nahum22 <nahum.nahum@gmail.com>
2024-08-07 09:22:03 +0300

    small tweaks

--- src/CarouselPage.jsx
+++ src/CarouselPage.jsx
@@ -6,16 +6,13 @@ const CarouselPage = () => {
   return (
     <Carousel infiniteLoop autoPlay interval={2000}>
       <div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSDugGTO2QXfTjuAcO8aat5EhJ6053_xjTXD1d_0HrYmc1WUFCvn3qnLwk_T4vmODjqPs&usqp=CAU" />
-        <p className="legend">Legend 1</p>
+        <img src="carousaleImages/image1.png" />
       </div>
       <div>
-        <img src="https://ynet-pic1.yit.co.il/picserver5/wcm_upload/2021/06/01/ByxMLBIQqu/1111.JPG" />
-        <p className="legend">Legend 2</p>
+        <img src="carousaleImages/image2.png" />
       </div>
       <div>
-        <img src="https://www.idosport.co.il/wp-content/uploads/2021/04/Untitled-design.jpg" />
-        <p className="legend">Legend 3</p>
+        <img src="carousaleImages/image3.png" />
       </div>
     </Carousel>
   );
